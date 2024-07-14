892e8f159aec5d9277cb3eb0bf162b198ff34e7a
praneet <praneetkashyap001@gmail.com>
2024-07-14 10:34:00 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://praneetog:77818@cluster0.y8tzrtt.mongodb.net/payit')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {User}
