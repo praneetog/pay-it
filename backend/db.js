90f800027922e86e9b0dd691d3a58daefe4d1768
praneet <praneetkashyap001@gmail.com>
2024-08-21 15:11:08 +0530

    updates

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb+srv://praneetog:77818@cluster0.gufva.mongodb.net/pay-it")
+
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
 
+const rootRouter = require("./routes/index");
+const userRouter = require("./routes/index");
+
+const app = express();
+
+app.use("api/v1", rootRouter);
+app.use("api/v1/user", userRouter);
+
+app.listen(3000);
